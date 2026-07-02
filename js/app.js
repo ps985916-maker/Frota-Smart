900aea6075e5090b356b9ba53900cd07125a97b4
ps985916-maker <ps985916@gmail.com>
2026-07-02 01:49:12 +0000

    Aula 1 a 7 - Estrutura inicial do Frota Inteligente

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-document.qSuerySelector("button").addEventListener("click",()=>{
+document.querySelector("button").addEventListener("click", function () {
 
-alert("Bem-vindo ao Frota Inteligente!");
+    alert("Bem-vindo ao Frota Inteligente!");
 
 });
